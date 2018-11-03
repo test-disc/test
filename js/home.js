a2af17e521d22d617f39b53bf0e563f1010afa39
Alex <aleksandra@kuzniak.com.pl>
2018-11-03 18:47:22 +0100

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,399 @@
+$(function ($) {
+
+    var languageButton = $('.language-button');
+
+    translateTest("en");
+    changeButtons($('.language-button-english'));
+
+    $(languageButton).click(function () {
+        if ($(this).hasClass('button-disabled'))
+            return;
+
+        var lang = $(this).attr("id");
+        translateTest(lang);
+
+        $(languageButton).each(function () {
+            changeButtons($(this));
+        })
+    });
+
+    function translateTest(lang) {
+        $(".language-text").each(function () {
+            $(this).text(arrLang[lang][$(this).attr("id")]);
+        });
+    }
+
+    function changeButtons(button) {
+        if ($(button).hasClass('button-disabled'))
+            $(button).removeClass('button-disabled');
+        else
+            $(button).addClass('button-disabled');
+    }
+
+    var iconYesSelected = ".icon-yes.selected";
+    var iconNoSelected = ".icon-no.selected";
+
+    $('.answer-icon').click(function () {
+        var other;
+        if ($(this).hasClass('not-selected')) {
+            if ($(this).hasClass('icon-yes')) {
+                $(this).attr("src", "content/icons/icon-yes-selected.png");
+                other = $(this).parent().parent().find(iconNoSelected);
+                if ($(other).length > 0) {
+                    changeSelection($(other), false);
+                }
+                other = $(this).parent().parent().parent().find(iconYesSelected);
+                if ($(other).length > 0) {
+                    changeSelection($(other), true);
+                }
+            } else {
+                $(this).attr("src", "content/icons/icon-no-selected.png");
+                other = $(this).parent().parent().find(iconYesSelected);
+                if ($(other).length > 0) {
+                    changeSelection($(other), true);
+
+                }
+                other = $(this).parent().parent().parent().find(iconNoSelected);
+                if ($(other).length > 0) {
+                    changeSelection($(other), false);
+                }
+            }
+
+            $(this).removeClass('not-selected');
+            $(this).addClass('selected');
+
+        } else {
+            if ($(this).hasClass('icon-yes'))
+                changeSelection($(this), true);
+            else
+                changeSelection($(this), false);
+        }
+    });
+
+    $('.save-test-button').click(function () {
+        var mostMap = new Map();
+        var leastMap = new Map();
+        var answered = 0;
+
+        var selected = $('.test-box').find('.selected');
+        $(selected).each(function (index, el) {
+            var value = $(el).attr('data-val');
+            var number;
+            if ($(el).hasClass('icon-yes')) {
+                number = parseInt(mostMap.get(value));
+                if (isNaN(number)) {
+                    mostMap.set(value, 1);
+                } else {
+                    number = number + 1;
+                    mostMap.set(value, number);
+                }
+            } else {
+                number = parseInt(leastMap.get(value));
+                if (isNaN(number)) {
+                    leastMap.set(value, 1);
+                } else {
+                    number = number + 1;
+                    leastMap.set(value, number);
+                }
+            }
+            answered = answered + 1;
+        });
+
+        if (answered !== 48) {
+            //console.log("NOT ALL ARE ANSWERED");
+            $('.question-box').each(function (index, el) {
+                var selected = $(el).find('.selected');
+                if ($(selected).length < 2) {
+                    $(el).addClass('missing-answers')
+                } else {
+                    $(el).removeClass('missing-answers')
+                }
+            });
+            return;
+        } else {
+            $('.question-box.missing-answers').removeClass('missing-answers')
+        }
+
+        addValues(leastMap, mostMap);
+
+        var testContent = $('.test-content');
+        var resultContent = $('.results-content');
+        testContent.empty();
+        $(resultContent).css("display", "flex");
+        showCharts();
+    });
+
+    function addValues(leastMap, mostMap) {
+        $('#red-most-value').val(getMostRed(mostMap.get(1)));
+        $('#yellow-most-value').val(getMostYellow(mostMap.get(2)));
+        $('#green-most-value').val(getMostGreen(mostMap.get(3)));
+        $('#blue-most-value').val(getMostBlue(mostMap.get(4)));
+        $('#red-least-value').val(getLeastRed(leastMap.get(1)));
+        $('#yellow-least-value').val(getLeastYellow(leastMap.get(2)));
+        $('#green-least-value').val(getLeastGreen(leastMap.get(3)));
+        $('#blue-least-value').val(getLeastBlue(leastMap.get(4)));
+    }
+
+    function changeSelection(selection, isYes) {
+        var icon;
+        if (isYes) {
+            icon = "content/icons/icon-yes.png";
+        } else {
+            icon = "content/icons/icon-no.png";
+        }
+        $(selection).removeClass('selected');
+        $(selection).addClass('not-selected');
+        $(selection).attr("src", icon);
+    }
+
+    function showCharts() {
+        showChart($('#results-box-most'), $('#red-most-value').val(), $('#yellow-most-value').val(),
+            $('#green-most-value').val(), $('#blue-most-value').val(), "Adapted Style");
+        showChart($('#results-box-least'), $('#red-least-value').val(), $('#yellow-least-value').val(),
+            $('#green-least-value').val(), $('#blue-least-value').val(), "Basic Style");
+    }
+
+    function showChart(placeholder, red, yellow, green, blue, title) {
+        var myChart = new Chart($(placeholder), {
+            type: 'bar',
+            data: {
+                labels: ["Red: " + red, "Yellow: " + yellow, "Green: " + green,
+                    "Blue: " + blue],
+                datasets: [{
+                    data: [red, yellow, green, blue],
+                    backgroundColor: [
+                        'rgba(217, 85, 79, 0.2)',
+                        'rgba(249, 186, 90, 0.2)',
+                        'rgba(90, 184, 143, 0.2)',
+                        'rgba(31, 91, 164, 0.2)'
+                    ]
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    responsive: true,
+                    display: true,
+                    text: title
+                },
+                animation: {
+                    onComplete: done
+                },
+                scales: {
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            beginAtZero: true,
+                            steps: 5,
+                            stepValue: 5,
+                            max: 100
+                        }
+                    }]
+                }
+            }
+        });
+
+        function done() {
+            var pictureHolder = $(placeholder).attr('id') + '-picture';
+            $('#' + pictureHolder).attr('src', myChart.toBase64Image());
+            $(placeholder).css('display', 'none');
+        }
+    }
+});
+
+function getMostRed(selected) {
+    var map = new Map();
+    map.set(20, 100.0);
+    map.set(19, 95.82);
+    map.set(18, 95.82);
+    map.set(17, 95.82);
+    map.set(16, 95.82);
+    map.set(15, 92.14);
+    map.set(14, 88.45);
+    map.set(13, 84.77);
+    map.set(12, 81.08);
+    map.set(11, 77.40);
+    map.set(10, 73.71);
+    map.set(9, 70.02);
+    map.set(8, 63.70);
+    map.set(7, 56.76);
+    map.set(6, 47.91);
+    map.set(5, 41.15);
+    map.set(4, 33.78);
+    map.set(3, 30.10);
+    map.set(2, 22.73);
+    map.set(1, 12.29);
+    map.set(0, 0.0);
+    return map.get(selected);
+}
+
+function getMostYellow(selected) {
+    var map = new Map();
+    map.set(18, 100.0);
+    map.set(17, 100.0);
+    map.set(16, 92.14);
+    map.set(15, 92.14);
+    map.set(14, 92.14);
+    map.set(13, 92.14);
+    map.set(12, 92.14);
+    map.set(11, 92.14);
+    map.set(10, 92.14);
+    map.set(9, 88.45);
+    map.set(8, 84.77);
+    map.set(7, 77.40);
+    map.set(6, 67.20);
+    map.set(5, 60.20);
+    map.set(4, 51.84);
+    map.set(3, 41.15);
+    map.set(2, 30.10);
+    map.set(1, 15.60);
+    map.set(0, 0.0);
+    return map.get(selected);
+}
+
+function getMostGreen(selected) {
+    var map = new Map();
+    map.set(19, 100.0);
+    map.set(18, 95.82);
+    map.set(17, 95.82);
+    map.set(16, 95.82);
+    map.set(15, 95.82);
+    map.set(14, 95.82);
+    map.set(13, 95.82);
+    map.set(12, 95.82);
+    map.set(11, 92.14);
+    map.set(10, 84.77);
+    map.set(9, 77.40);
+    map.set(8, 73.71);
+    map.set(7, 67.20);
+    map.set(6, 67.20);
+    map.set(5, 56.76);
+    map.set(4, 47.91);
+    map.set(3, 41.15);
+    map.set(2, 30.10);
+    map.set(1, 22.73);
+    map.set(0, 12.04);
+    return map.get(selected);
+}
+
+function getMostBlue(selected) {
+    var map = new Map();
+    /*    map.set(15, 100.0);
+        map.set(14, 95.82);
+        map.set(13, 95.82);
+        map.set(12, 95.82);
+        map.set(11, 95.82);
+        map.set(10, 95.82);
+        map.set(9, 95.82);
+        map.set(8, 92.14);
+        map.set(7, 88.45);
+        map.set(6, 77.40);
+        map.set(5, 67.20);
+        map.set(4, 56.76);
+        map.set(3, 44.47);
+        map.set(2, 33.66);
+        map.set(1, 15.60);
+        map.set(0, 00.00);*/
+    return map.get(selected);
+}
+
+function getLeastRed(selected) {
+    var map = new Map();
+    map.set(0, 100.0);
+    map.set(1, 92.14);
+    map.set(2, 81.08);
+    map.set(3, 73.71);
+    map.set(4, 67.20);
+    map.set(5, 67.20);
+    map.set(6, 51.84);
+    map.set(7, 44.47);
+    map.set(8, 41.15);
+    map.set(9, 37.71);
+    map.set(10, 33.66);
+    map.set(11, 30.10);
+    map.set(12, 27.25);
+    map.set(13, 20.07);
+    map.set(14, 15.60);
+    map.set(15, 12.04);
+    map.set(16, 9.49);
+    map.set(17, 0.0);
+    map.set(18, 0.0);
+    map.set(19, 0.0);
+    map.set(20, 0.0);
+    map.set(21, 0.0);
+    return map.get(selected);
+}
+
+function getLeastYellow(selected) {
+    var map = new Map();
+    map.set(0, 100.0);
+    map.set(1, 88.45);
+    map.set(2, 77.40);
+    map.set(3, 70.02);
+    map.set(4, 67.20);
+    map.set(5, 51.84);
+    map.set(6, 44.47);
+    map.set(7, 37.71);
+    map.set(8, 30.10);
+    map.set(9, 22.73);
+    map.set(10, 20.07);
+    map.set(11, 15.60);
+    map.set(12, 9.49);
+    map.set(13, 9.49);
+    map.set(14, 9.49);
+    map.set(15, 9.49);
+    map.set(16, 0.00);
+    map.set(17, 0.00);
+    map.set(18, 0.00);
+    map.set(19, 0.00);
+    return map.get(selected);
+}
+
+function getLeastGreen(selected) {
+    var map = new Map();
+    map.set(0, 100.0);
+    map.set(1, 95.82);
+    map.set(2, 88.45);
+    map.set(3, 77.40);
+    map.set(4, 70.02);
+    map.set(5, 63.70);
+    map.set(6, 56.76);
+    map.set(7, 47.91);
+    map.set(8, 41.15);
+    map.set(9, 33.66);
+    map.set(10, 27.25);
+    map.set(11, 20.07);
+    map.set(12, 12.04);
+    map.set(13, 9.49);
+    map.set(14, 0.00);
+    map.set(15, 0.00);
+    map.set(16, 0.00);
+    map.set(17, 0.00);
+    map.set(18, 0.00);
+    map.set(19, 0.00);
+    return map.get(selected);
+}
+
+function getLeastBlue(selected) {
+    var map = new Map();
+    map.set(0, 100.0);
+    map.set(1, 95.82);
+    map.set(2, 84.77);
+    map.set(3, 77.40);
+    map.set(4, 70.02);
+    map.set(5, 63.70);
+    map.set(6, 56.76);
+    map.set(7, 51.84);
+    map.set(8, 41.15);
+    map.set(9, 37.71);
+    map.set(10, 27.25);
+    map.set(11, 20.07);
+    map.set(12, 12.04);
+    map.set(13, 9.49);
+    map.set(14, 0.00);
+    map.set(15, 0.00);
+    map.set(16, 0.00);
+    return map.get(selected);
+}
---
+++ js/languages.js
@@ -0,0 +1,244 @@
+var arrLang = {
+    "pl": {
+        "test_box0_question0": "Ufna, entuzjastyczna",
+        "test_box0_question1": "Tolerancyjna, pełna szacunku",
+        "test_box0_question2": "Odważna, śmiała",
+        "test_box0_question3": "Zgodna, uczynna",
+
+        "test_box1_question0": "Innowacyjna, wizjonerka",
+        "test_box1_question1": "Ostrożna, małomówna",
+        "test_box1_question2": "Towarzyska, miła",
+        "test_box1_question3": "Mediatorka, negocjatorka",
+
+        "test_box2_question0": "Dokładna, precyzyjna",
+        "test_box2_question1": "Skupiona, ukierunkowana na cel",
+        "test_box2_question2": "Gracz zespołowy, uczynna",
+        "test_box2_question3": "Zachęcająca innych, stymulująca",
+
+        "test_box3_question0": "Wrażliwa, bywa sfrustrowana",
+        "test_box3_question1": "Gotowa się przeciwstawiać, prawa",
+        "test_box3_question2": "Skryta, ufa swojemu osądowi",
+        "test_box3_question3": "Ma swoje zdanie, chce być usłyszana",
+
+        "test_box4_question0": "Spokojna, szuka równowagi",
+        "test_box4_question1": "Gadatliwa, charyzmatyczna",
+        "test_box4_question2": "Przestrzega reguł, poukładana",
+        "test_box4_question3": "Szybka, pełna energii",
+
+        "test_box5_question0": "Systematyczna, zarządza czasem",
+        "test_box5_question1": "Niespokojna, zalatana",
+        "test_box5_question2": "Godna zaufania, zawzięta",
+        "test_box5_question3": "Uczuciowa, impulsywna",
+
+        "test_box6_question0": "Bezstronna, zbyt ostrożna",
+        "test_box6_question1": "Ufa swojemu osądowi, nie lubi zmian",
+        "test_box6_question2": "Dosadna, apodyktyczna",
+        "test_box6_question3": "Mało realistyczna, podejmuje się niewykonalnych zadań",
+
+        "test_box7_question0": "Dobrze analizuje",
+        "test_box7_question1": "Jest dobrym słuchaczem",
+        "test_box7_question2": "Potrafi zachęcić innych",
+        "test_box7_question3": "Jest dobrym delegatorem",
+
+        "test_box8_question0": "Dynamiczna, ambitna",
+        "test_box8_question1": "Optymistyczna, charyzmatyczna",
+        "test_box8_question2": "Chętna do współpracy, kooperatywna",
+        "test_box8_question3": "Dokładna, precyzyjna",
+
+        "test_box9_question0": "Czeka z zakupem, cierpliwa",
+        "test_box9_question1": "Kupuje impulsowo, stanowcza",
+        "test_box9_question2": "Wydaje na to co chce, samolubna",
+        "test_box9_question3": "Obejdzie się bez zakupów, ma dużo samokontroli",
+
+        "test_box10_question0": "Nieugięta, potrzebuje konkretów",
+        "test_box10_question1": "Unika monotonii, nudzi ją rutyna",
+        "test_box10_question2": "Pragnie zmian, idzie na całość",
+        "test_box10_question3": "Miła, życzliwa",
+
+        "test_box11_question0": "Impulsywna, uczuciowa",
+        "test_box11_question1": "Kalkuluje, zwraca uwagę na szczegóły",
+        "test_box11_question2": "Wymagająca, apodyktyczna",
+        "test_box11_question3": "Nie lubi konfrontacji, przewidywalna",
+
+        "test_box12_question0": "Uczuciowa, delikatna",
+        "test_box12_question1": "Skromna, zgodna",
+        "test_box12_question2": "Zdeterminowana, chce wyników",
+        "test_box12_question3": "Usatysfakcjonowana, spełniona",
+
+        "test_box13_question0": "Nieustraszona, niezależna",
+        "test_box13_question1": "Ostrożna, rozważna",
+        "test_box13_question2": "Beztroska, nieostrożna",
+        "test_box13_question3": "Życzliwa, ciepła",
+
+        "test_box14_question0": "Sumienna, planuje przyszłość",
+        "test_box14_question1": "Szuka postępów, chce uznania",
+        "test_box14_question2": "Śmiała, ryzykowna",
+        "test_box14_question3": "Godna zaufania, dobry słuchacz",
+
+        "test_box15_question0": "Reguły ją nudzą, niespokojna",
+        "test_box15_question1": "Kwestionuje reguły, odważna",
+        "test_box15_question2": "Reguły dają bezpieczeństwo",
+        "test_box15_question3": "Reguły dają poczucie sprawiedliwości",
+
+        "test_box16_question0": "Lubi nagrody i osiągnięcia",
+        "test_box16_question1": "Lubi towarzystwo i spotkania",
+        "test_box16_question2": "Kulturalna, lubi edukację",
+        "test_box16_question3": "Pragnie bezpieczeństwa",
+
+        "test_box17_question0": "Rozważna, wszystko kalkuluje",
+        "test_box17_question1": "Konsekwentna, dokładna",
+        "test_box17_question2": "Otwarta, entuzjastyczna",
+        "test_box17_question3": "Bierze sprawy w swoje ręce, ma bezpośrednie podejście",
+
+        "test_box18_question0": "Nadpobudliwa, radosna",
+        "test_box18_question1": "Wspierająca, typ adwokata",
+        "test_box18_question2": "Metodyczna, konkretna",
+        "test_box18_question3": "Lubi rywalizację, rzeczowa",
+
+        "test_box19_question0": "Bazuje na faktach",
+        "test_box19_question1": "Podąża za innymi",
+        "test_box19_question2": "Umie przewodzić grupie",
+        "test_box19_question3": "Przekonuje do swoich racji",
+
+        "test_box20_question0": "Lojalna, refleksyjna",
+        "test_box20_question1": "Lubi wyzwania, pionierska",
+        "test_box20_question2": "Analityczna, taktowna",
+        "test_box20_question3": "Popularna, przekonująca",
+
+        "test_box21_question0": "Zgodna, ustępliwa",
+        "test_box21_question1": "Pobudzona, żywiołowa",
+        "test_box21_question2": "Nieustraszona, odważna",
+        "test_box21_question3": "Zorganizowana, dopasowuje się",
+
+        "test_box22_question0": "Autoratytatywna, mająca wpływ",
+        "test_box22_question1": "Pragnie uwagi, lubi wyzwania",
+        "test_box22_question2": "Unika konfliktów, wyluzowana",
+        "test_box22_question3": "Dyplomatyczna, przestrzega reguł",
+
+        "test_box23_question0": "Kreatywna, wyjątkowa",
+        "test_box23_question1": "Godna zaufania, autentyczna",
+        "test_box23_question2": "Organizatorka, zorientowana na efekt",
+        "test_box23_question3": "Odnosi się do wzorców, działa według standardów"
+    },
+    "en": {
+        "test_box0_question0": "question 1 text in english",
+        "test_box0_question1": "question 2 text in english",
+        "test_box0_question2": "question 3 text in english",
+        "test_box0_question3": "question 4 text in english",
+
+        "test_box1_question0": "",
+        "test_box1_question1": "",
+        "test_box1_question2": "",
+        "test_box1_question3": "",
+
+        "test_box2_question0": "",
+        "test_box2_question1": "",
+        "test_box2_question2": "",
+        "test_box2_question3": "",
+
+        "test_box3_question0": "",
+        "test_box3_question1": "",
+        "test_box3_question2": "",
+        "test_box3_question3": "",
+
+        "test_box4_question0": "",
+        "test_box4_question1": "",
+        "test_box4_question2": "",
+        "test_box4_question3": "",
+
+        "test_box5_question0": "",
+        "test_box5_question1": "",
+        "test_box5_question2": "",
+        "test_box5_question3": "",
+
+        "test_box6_question0": "",
+        "test_box6_question1": "",
+        "test_box6_question2": "",
+        "test_box6_question3": "",
+
+        "test_box7_question0": "",
+        "test_box7_question1": "",
+        "test_box7_question2": "",
+        "test_box7_question3": "",
+
+        "test_box8_question0": "",
+        "test_box8_question1": "",
+        "test_box8_question2": "",
+        "test_box8_question3": "",
+
+        "test_box9_question0": "",
+        "test_box9_question1": "",
+        "test_box9_question2": "",
+        "test_box9_question3": "",
+
+        "test_box10_question0": "",
+        "test_box10_question1": "",
+        "test_box10_question2": "",
+        "test_box10_question3": "",
+
+        "test_box11_question0": "",
+        "test_box11_question1": "",
+        "test_box11_question2": "",
+        "test_box11_question3": "",
+
+        "test_box12_question0": "",
+        "test_box12_question1": "",
+        "test_box12_question2": "",
+        "test_box12_question3": "",
+
+        "test_box13_question0": "",
+        "test_box13_question1": "",
+        "test_box13_question2": "",
+        "test_box13_question3": "",
+
+        "test_box14_question0": "",
+        "test_box14_question1": "",
+        "test_box14_question2": "",
+        "test_box14_question3": "",
+
+        "test_box15_question0": "",
+        "test_box15_question1": "",
+        "test_box15_question2": "",
+        "test_box15_question3": "",
+
+        "test_box16_question0": "",
+        "test_box16_question1": "",
+        "test_box16_question2": "",
+        "test_box16_question3": "",
+
+        "test_box17_question0": "",
+        "test_box17_question1": "",
+        "test_box17_question2": "",
+        "test_box17_question3": "",
+
+        "test_box18_question0": "",
+        "test_box18_question1": "",
+        "test_box18_question2": "",
+        "test_box18_question3": "",
+
+        "test_box19_question0": "",
+        "test_box19_question1": "",
+        "test_box19_question2": "",
+        "test_box19_question3": "",
+
+        "test_box20_question0": "",
+        "test_box20_question1": "",
+        "test_box20_question2": "",
+        "test_box20_question3": "",
+
+        "test_box21_question0": "",
+        "test_box21_question1": "",
+        "test_box21_question2": "",
+        "test_box21_question3": "",
+
+        "test_box22_question0": "",
+        "test_box22_question1": "",
+        "test_box22_question2": "",
+        "test_box22_question3": "",
+
+        "test_box23_question0": "",
+        "test_box23_question1": "",
+        "test_box23_question2": "",
+        "test_box23_question3": ""
+    }
+}
