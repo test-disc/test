6f992a5e43f20a4c286d02706a5fc855593e419c
test-disc <44726543+test-disc@users.noreply.github.com>
2020-02-19 16:59:40 +0100

    Update languages.js

--- js/languages.js
+++ js/languages.js
@@ -133,8 +133,8 @@ var arrLang = {
 
         "error-box": "Nie odpowiedziałaś na wszystkie pytania!",
         "save-test-button": "Zakończ test!",
-        "disclaimer-text": "Jeśli chcesz uzyskać więcej informacji na temat swojego testu wejdź na www.koodoo.pl. " + 
-        "Pamiętaj, żeby zrobić screen z wynikami testu!"
+        "disclaimer-text1": "Jeśli chcesz uzyskać więcej informacji na temat swojego testu wejdź na "
+        "disclaimer-text2": " Pamiętaj, żeby zrobić screen z wynikami testu!"
     },
     "en": {
         "test_title":"DISC Personality Profile",
@@ -269,6 +269,6 @@ var arrLang = {
 
         "error-box": "Not all questions are answered!",
         "save-test-button": "Finish test!",
-        "disclaimer-text": "If you want to get more information about your results, screenshot them and go to www.koodoo.pl"
+        "disclaimer-text1": "If you want to get more information about your results, screenshot them and go to "
     }
 }
